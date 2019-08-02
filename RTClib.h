4bb108390d401696379b9e1b71a5404192a6ac6c
saifuhameed <53597785+saifuhameed@users.noreply.github.com>
2019-08-02 11:05:08 +0300

    Update RTClib.h

--- RTClib.h
+++ RTClib.h
@@ -53,7 +53,7 @@ class DateTime {
 public:
   DateTime (uint32_t t = SECONDS_FROM_1970_TO_2000);
   DateTime (uint16_t year, uint8_t month, uint8_t day,
-              uint8_t hour = 0, uint8_t min = 0, uint8_t sec = 0, uint8_t hmode = 0, uint8_t ampm = 0 );
+              uint8_t hour = 0, uint8_t min = 0, uint8_t sec = 0, uint8_t _hourmode = 0, uint8_t _ampm = 0 );
   DateTime (const DateTime& copy);
   DateTime (const char* date, const char* time);
   DateTime (const __FlashStringHelper* date, const __FlashStringHelper* time);
